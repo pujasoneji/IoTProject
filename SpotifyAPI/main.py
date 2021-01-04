0963f94dc82ef12fe6bb65f48360d17ca512b470
pujasoneji <pujasoneji@yahoo.co.uk>
2021-01-04 11:26:35 +0000

    update to code: error handling for no song playing

--- SpotifyAPI/main.py
+++ SpotifyAPI/main.py
@@ -37,40 +37,44 @@ while True: #code repeats indefinitely
 	sp = spotipy.Spotify(auth_manager=token)
 
 	currentInfo = sp.current_user_playing_track()
-	song = currentInfo['item']['name']
-	x = currentInfo['item']['album']['artists']
-	artist = [sub['name'] for sub in x]
-	y = currentInfo['item']['album']['images'][1]
-	image = y.get("url")
-	#image = [sub['url'] for sub in y]
 
-	if len(songList) == 0: #if this is the first entry to the dictionary. This avoids us checking if this song is the same as the last song
-		songInfo = (song, artist[0], image) #creating a list format to put into dictionary
-		ts = time.time() #timestamp for current song
-
-		addSongsToList(songList, int(ts), songInfo) #adding song information to the dictionary
-		print(songList)
-		time.sleep(sleep) #sleep time before loop continues
+	if currentInfo is None:
+		time.sleep(sleep)
 
 	else:
+		song = currentInfo['item']['name']
+		x = currentInfo['item']['album']['artists']
+		artist = [sub['name'] for sub in x]
+		y = currentInfo['item']['album']['images'][1]
+		image = y.get("url")
+		#image = [sub['url'] for sub in y]
 
-
-		lastSongInfo = list(songList)[count] #finding the name of the last key
-		lastSong = songList[lastSongInfo][0] #finding the associated song value for that key
-
-		if song != lastSong: #avoiding repeats in the dictionary
+		if len(songList) == 0: #if this is the first entry to the dictionary. This avoids us checking if this song is the same as the last song
 			songInfo = (song, artist[0], image) #creating a list format to put into dictionary
 			ts = time.time() #timestamp for current song
-			count = count + 1 #only adding 1 to count if a song has been added
 
 			addSongsToList(songList, int(ts), songInfo) #adding song information to the dictionary
 			print(songList)
 			time.sleep(sleep) #sleep time before loop continues
 
 		else:
-			time.sleep(sleep) #if song is still the same, nothing is executed. sleep time before we try again.
-		#print(ts)
-		#print("You are currently playing " + song + " by " + artist[0])
+
+
+			lastSongInfo = list(songList)[count] #finding the name of the last key
+			lastSong = songList[lastSongInfo][0] #finding the associated song value for that key
+
+			if song != lastSong: #avoiding repeats in the dictionary
+				songInfo = (song, artist[0], image) #creating a list format to put into dictionary
+				ts = time.time() #timestamp for current song
+				count = count + 1 #only adding 1 to count if a song has been added
+
+				addSongsToList(songList, int(ts), songInfo) #adding song information to the dictionary
+				print(songList)
+				time.sleep(sleep) #sleep time before loop continues
+
+			else:
+				time.sleep(sleep) #if song is still the same, nothing is executed. sleep time before we try again.
+			#print(ts)
+			#print("You are currently playing " + song + " by " + artist[0])
 	
 
-1 
